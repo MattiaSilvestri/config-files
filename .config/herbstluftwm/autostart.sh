f964e205d953140f57197e15dba23d63e6aa0aa0
MattiaSilvestri <mattiasilvestri27@gmail.com>
2021-12-26 16:30:03 +0100

    Add polybar config

---
+++ .config/herbstluftwm/autostart.sh
@@ -0,0 +1,31 @@
+
+
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+run "xinput set-prop "SYNA7DB5:00 06CB:CD41 Touchpad" "libinput Tapping Enabled" 1"
+run "conky -c /home/mattia/.config/conky/conky_sidepane2.conf"
+run "conky -c /home/mattia/.config/conky/calendar.conf"
+# run "conky -c /home/mattia/.config/conky/conkyKeys.conf"
+# run "pamac-tray"
+run "xfce4-power-manager"
+run "blueman-applet"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+run "numlockx off"
+run "volumeicon"
+run "nitrogen --restore"
+#run "megasync"
+run "redshift-gtk"
+#run "conky -c /home/mattia/.config/conky/WO-conky-system-lua-v3.conkyrc"
+# run "conky -c /home/mattia/.config/conky/conkyDate.conf"
+#run "conky -c /home/mattia/.config/conky/conkyCPU2.conf"
+# run "conky -c /home/mattia/.config/conky/calendar.conf"
+run "/usr/bin/setxkbmap -option "caps:swapescape""
+run "nm-applet"
+# run "emacs --daemon"
+run "picom --experimental-backends --config  $HOME/.config/awesome/configuration/picom.conf"
