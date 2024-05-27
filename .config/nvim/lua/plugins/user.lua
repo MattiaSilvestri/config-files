95f27bb1c14e67ce27fe63fdd3d3de89e0c7ae9a
MattiaSilvetri <mattiasilvestri27@gmail.com>
2024-05-27 11:46:00 +0200

    set global bookmarks and change arrow save dir

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -319,9 +319,11 @@ return {
     "otavioschwanck/arrow.nvim",
     lazy = false,
     opts = {
+      save_path = function() return vim.fn.stdpath "config" .. "/arrow" end,
       show_icons = true,
       leader_key = ";", -- Recommended to be a single key
       buffer_leader_key = "m", -- Per Buffer Mappings
+      global_bookmarks = true,
     },
   },
   -- {
