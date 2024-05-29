ddad0b63eca353ff2e1fd74f485a41532c737eb2
MattiaSilvetri <mattiasilvestri27@gmail.com>
2024-05-29 15:53:40 +0200

    add proper clipboard manager

--- .local/bin/volume_mute.sh
+++ .local/bin/volume_mute.sh
@@ -12,12 +12,12 @@ volume="$(pamixer --get-volume)"
 mute="$(pactl get-sink-mute @DEFAULT_SINK@)"
 if [[ $volume == 0 || "$mute" == "Mute: yes" ]]; then
     # Show the sound muted notification
-    icon_name="~/.icons/Controls/silent.png"
+    icon_name="~/.icons/Controls/mute2.png"
     dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag \
         -i $icon_name "Volume muted" 
 else
     # Show the volume notification
-	icon_name="~/.icons/Controls/volume-up.png"
+	icon_name="~/.icons/Controls/high-volume.png"
     dunstify -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
     -i $icon_name -h int:value:"$volume" "${volume}%"
 fi
