f4243241d7962d3a47fd99cfb2a8a3991e64b859
Mattia Silvetri <mattiasilvestri27@gmail.com>
2025-02-05 17:29:35 +0100

    bspwm

---
+++ .config/bspwm.0/polybar/forest/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.dotfiles/.config/bspwm.0/polybar/forest"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+# MONITOR=eDP-1 polybar -q main -c "$DIR"/config_eDP-1.ini &
+# MONITOR=HDMI-1 polybar -q main -c "$DIR"/config_HDMI.ini &
+
