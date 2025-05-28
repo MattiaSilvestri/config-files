f40f72c45914e2872853237b1a97b6fb66b77abd
Mattia Silvetri <mattiasilvestri27@gmail.com>
2025-05-28 16:25:37 +0200

    neovide

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,11 +2,11 @@ return {
 	colorscheme = "nvchad",
 	opt = {
 		expandtab = true,
-		relativenumber = true, -- sets vim.opt.relativenumber
-		number = true, -- sets vim.opt.number
-		wrap = false, -- LIne wrawpping
-		laststatus = 3, -- Statusline spans all splits
-		cursorline = true, -- Enable highlighting of the current line
+		relativenumber = true,   -- sets vim.opt.relativenumber
+		number = true,           -- sets vim.opt.number
+		wrap = false,            -- LIne wrawpping
+		laststatus = 3,          -- Statusline spans all splits
+		cursorline = true,       -- Enable highlighting of the current line
 		clipboard = "unnamedplus", -- Allows neovim to access the system clipboard
 		tabstop = 2,
 		shiftwidth = 0,
@@ -14,6 +14,7 @@ return {
 		copyindent = true,
 		fillchars = { eob = " " },
 		timeoutlen = 100,
+		guifont = "FiraCode Nerd Font:h9:w0:#h-normal",
 	},
 	o = {
 		-- fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldclose:",
@@ -24,5 +25,17 @@ return {
 		-- foldexpr = "v:lua.vim.lsp.foldexpr()",
 		-- foldmethod = "expr",
 	},
-	g = {},
+	g = {
+		neovide_padding_top = 6,
+		neovide_padding_bottom = 4,
+		neovide_padding_right = 6,
+		neovide_padding_left = 6,
+		neovide_floating_blur_amount_x = 5.0,
+		neovide_floating_blur_amount_y = 5.0,
+		neovide_floating_corner_radius = 0.3,
+		neovide_cursor_animation_length = 0,
+		neovide_cursor_vfx_mode = "",
+		neovide_text_gamma = 1.3,
+		neovide_text_contrast = 0.3,
+	},
 }
