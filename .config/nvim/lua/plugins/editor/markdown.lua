732060245015531ea39c1571a54e1b5db512ceda
Mattia Silvetri <mattiasilvestri27@gmail.com>
2026-04-08 14:02:39 +0200

    markdown

--- .config/nvim/lua/plugins/editor/markdown.lua
+++ .config/nvim/lua/plugins/editor/markdown.lua
@@ -3,15 +3,20 @@ return {
 		"OXY2DEV/markview.nvim",
 		enabled = false,
 		lazy = false,
-
-		-- For blink.cmp's completion
-		-- source
+		ft = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
+		-- For blink.cmp's completion source
 		dependencies = {
 			"saghen/blink.cmp",
+		},
+		opts = {
+			filetypes = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
+			buf_ignore = {},
+			max_length = 99999,
 		},
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
+		enabled = true,
 		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
