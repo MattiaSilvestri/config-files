70067f5f3f9a1182dc4bd841b0fd6c89cc5314f3
Mattia Silvetri <mattiasilvestri27@gmail.com>
2026-06-03 13:28:00 +0200

    hyprexpo

--- .config/hypr/config/mappings.lua
+++ .config/hypr/config/mappings.lua
@@ -1,5 +1,45 @@
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
+------------------
+----  SHELL  ----
+------------------
+-- Laptop multimedia keys for volume and LCD brightness
+hl.bind(
+	"XF86AudioRaiseVolume",
+	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioLowerVolume",
+	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMicMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86MonBrightnessUp",
+	hl.dsp.exec_cmd("dms ipc call brightness increment 5 '' "),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86MonBrightnessDown",
+	hl.dsp.exec_cmd("dms ipc call brightness decrement 5 '' "),
+	{ locked = true, repeating = true }
+)
+
+-- Requires playerctl
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("dms ipc call mpris next"), { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("dms ipc call mpris playPause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("dms ipc call mpris previous"), { locked = true })
+
 hl.bind(mainMod .. " + left", hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
@@ -24,7 +64,7 @@ hl.bind("ALT + TAB", hl.dsp.group.next())
 hl.bind("ALT + SHIFT + TAB", hl.dsp.group.prev())
 
 -------------------
----- HYMISION ----
+---- HYMISSION ----
 ------------------
 
 hl.plugin.hymission.gesture({
@@ -77,3 +117,28 @@ hl.bind("SUPER + Escape", hl.plugin.hymission.close)
 hl.bind(mainMod .. " + SHIFT + period", hl.dsp.layout("consume_or_expel next"))
 hl.bind(mainMod .. " + SHIFT + comma", hl.dsp.layout("consume_or_expel prev"))
 hl.bind(mainMod .. " + R", hl.dsp.layout("colresize +conf"))
+
+hl.bind("SUPER + P", function()
+	hl.plugin.hyprexpo.expo("toggle")
+end)
+
+hl.define_submap("hyprexpo", function()
+	hl.bind("h", function()
+		hl.plugin.hyprexpo.kb_focus("left")
+	end)
+	hl.bind("l", function()
+		hl.plugin.hyprexpo.kb_focus("right")
+	end)
+	hl.bind("k", function()
+		hl.plugin.hyprexpo.kb_focus("up")
+	end)
+	hl.bind("j", function()
+		hl.plugin.hyprexpo.kb_focus("down")
+	end)
+	hl.bind("return", function()
+		hl.plugin.hyprexpo.kb_confirm()
+	end)
+	hl.bind("escape", function()
+		hl.plugin.hyprexpo.expo("cancel")
+	end)
+end)
